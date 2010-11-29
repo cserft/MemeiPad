03006efe2923c4c9b15dc2765b90c0c297d37973
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-11-29 20:33:05 -0200

    If highlights are already opened, dont's open them again.

--- Resources/app_highlight.js
+++ Resources/app_highlight.js
@@ -28,102 +28,108 @@ var getHighlightQuery = function(callback) {
 	xhr.send();
 };
 
+var highlightsDisplayed = false;
 var getHighlights = function (highlightView) {
 	
-	getHighlightQuery(function (data) {
+	if (! highlightsDisplayed) {
 		
-		var yql_data = Ti.App.oAuthAdapter.getYql().query("SELECT * FROM meme.search(" + data.amount + ") WHERE query='" + data.query + "'" );
-		
-		var posts = yql_data.query.results.post;
-		
-		for ( var i=0 ; i< posts.length ; i++ ) {
-			
-			// if doesn't have caption, move to next item
-			// we don't show posts without caption
-			if (!posts[i].caption || (strip_html_entities(posts[i].caption) == '')) {
-				continue;
-			}
-				
-			var highlight = Ti.UI.createView({
-				backgroundColor:'black',
-				width: 1024,
-				height: 273,
-				zIndex: 0
-			});
-		
-			// photo
-			var highlightPhoto = Ti.UI.createImageView({
-				image: 				posts[i].content,
-				backgroundColor: 	'black',
-				width: 				1024,
-				height: 			'auto',
-				zIndex: 			0
-			});
-			highlight.add(highlightPhoto);
+		getHighlightQuery(function (data) {
 
-		
-			var captionBgView = Ti.UI.createView({
-				backgroundColor:'black',
-				opacity: 0.9,
-				top: 150,
-				left: 634,
-				width: 390,
-				height: 87,
-				zIndex: 2
-			});
+			var yql_data = Ti.App.oAuthAdapter.getYql().query("SELECT * FROM meme.search(" + data.amount + ") WHERE query='" + data.query + "'" );
 
-			var featuredLabel = Ti.UI.createLabel({
-				color: 			'#ffffff',
-				text:  			'FEATURED CONTENT',
-				font: 			{fontSize:11, fontFamily:'Helvetica', fontWeight:'regular'},
-				opacity: 		0.5,
-				textAlign: 		'left',
-				top: 			10,
-				left:  			15,
-				height: 		20,
-				width: 			150,
-				zIndex: 		3
-			});
-			captionBgView.add(featuredLabel);
+			var posts = yql_data.query.results.post;
 
-			var captionLabel = Ti.UI.createLabel({
-				color: 			'#ffffff',
-				text:  			strip_html_entities(posts[i].caption),
-				font: 			{fontSize:18, fontFamily:'Helvetica', fontWeight:'bold'},
-				textAlign: 		'left',
-				top: 			30,
-				left:  			15,
-				height: 		50,
-				width: 			361,
-				zIndex: 		3
-			});
-			captionBgView.add(captionLabel);
-			
-			var boxLink = Ti.UI.createButton({
-				backgroundColor: 	'transparent',
-				width: 				'100%',
-				height: 			'100%',
-				zIndex: 			99,
-				guid: 				posts[i].guid,
-				pubId: 				posts[i].pubid,
-				backgroundSelectedImage: 	'images/btn_dashboard_link.png',
-				style: 						Titanium.UI.iPhone.SystemButtonStyle.PLAIN
-			});
-			captionBgView.add(boxLink);
-			
-			boxLink.addEventListener('click', function(e) {
-				Ti.App.fireEvent('openPermalink', {
-					guid: e.source.guid,
-					pubId: e.source.pubId
+			for ( var i=0 ; i< posts.length ; i++ ) {
+
+				// if doesn't have caption, move to next item
+				// we don't show posts without caption
+				if (!posts[i].caption || (strip_html_entities(posts[i].caption) == '')) {
+					continue;
+				}
+
+				var highlight = Ti.UI.createView({
+					backgroundColor:'black',
+					width: 1024,
+					height: 273,
+					zIndex: 0
 				});
-			});
 
-			highlight.add(captionBgView);
-			
-			// add to the scrollableview
-			highlightView.addView(highlight);
-		}
-	});
+				// photo
+				var highlightPhoto = Ti.UI.createImageView({
+					image: 				posts[i].content,
+					backgroundColor: 	'black',
+					width: 				1024,
+					height: 			'auto',
+					zIndex: 			0
+				});
+				highlight.add(highlightPhoto);
+
+
+				var captionBgView = Ti.UI.createView({
+					backgroundColor:'black',
+					opacity: 0.9,
+					top: 150,
+					left: 634,
+					width: 390,
+					height: 87,
+					zIndex: 2
+				});
+
+				var featuredLabel = Ti.UI.createLabel({
+					color: 			'#ffffff',
+					text:  			'FEATURED CONTENT',
+					font: 			{fontSize:11, fontFamily:'Helvetica', fontWeight:'regular'},
+					opacity: 		0.5,
+					textAlign: 		'left',
+					top: 			10,
+					left:  			15,
+					height: 		20,
+					width: 			150,
+					zIndex: 		3
+				});
+				captionBgView.add(featuredLabel);
+
+				var captionLabel = Ti.UI.createLabel({
+					color: 			'#ffffff',
+					text:  			strip_html_entities(posts[i].caption),
+					font: 			{fontSize:18, fontFamily:'Helvetica', fontWeight:'bold'},
+					textAlign: 		'left',
+					top: 			30,
+					left:  			15,
+					height: 		50,
+					width: 			361,
+					zIndex: 		3
+				});
+				captionBgView.add(captionLabel);
+
+				var boxLink = Ti.UI.createButton({
+					backgroundColor: 	'transparent',
+					width: 				'100%',
+					height: 			'100%',
+					zIndex: 			99,
+					guid: 				posts[i].guid,
+					pubId: 				posts[i].pubid,
+					backgroundSelectedImage: 	'images/btn_dashboard_link.png',
+					style: 						Titanium.UI.iPhone.SystemButtonStyle.PLAIN
+				});
+				captionBgView.add(boxLink);
+
+				boxLink.addEventListener('click', function(e) {
+					Ti.App.fireEvent('openPermalink', {
+						guid: e.source.guid,
+						pubId: e.source.pubId
+					});
+				});
+
+				highlight.add(captionBgView);
+
+				// add to the scrollableview
+				highlightView.addView(highlight);
+			}
+		});
+		
+		highlightsDisplayed = true;
+	}
 };
 
 
