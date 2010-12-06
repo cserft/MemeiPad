b770e649f6c61f1cc86acf92ba82ff280fe47716
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-12-05 23:40:20 -0200

    Refactoring on Meme module to provide better encapsulation.

--- Resources/lib/meme.js
+++ Resources/lib/meme.js
@@ -1,66 +1,91 @@
-var Meme = {};
+/***************************************************
+"Meme" module provides all functionality available
+on the Meme API.
 
-Meme.createTextPost = function(content) {
-	return Meme.internal.createPost('text', content);
-};
-
-Meme.createPhotoPost = function(content, caption) {
-	return Meme.internal.createPost('photo', content, caption);
-};
-
-Meme.createVideoPost = function(content, caption) {
-	return Meme.internal.createPost('video', content, caption);
-};
-
-Meme.isFollowing = function(guid) {
-	if (!Ti.App.oAuthAdapter.isLoggedIn()) {
-		throw 'Authentication is required to run this query.';
-	}
+Usage:
+	// follow an user
+	Meme.follow('guid_of_the_user_to_follow');
 	
-	var yqlQuery = 'SELECT * FROM meme.following WHERE owner_guid=me and guid="' + guid + '"';
-	var yqlResponse = Ti.App.oAuthAdapter.getYql().query(yqlQuery);
-	if (yqlResponse.query.results) {
-		return true;
-	}
-	return false;
-};
+	// creates a text post
+	Meme.createTextPost('A text post');
+***************************************************/
 
-Meme.follow = function(guid) {
-	var yqlQuery = 'INSERT INTO meme.user.following (guid) VALUES ("' + guid + '")';
-	return Meme.internal.execute(true, yqlQuery);
-};
+// Meme holds a reference to the result of the execution
+// of the following function (a.k.a. closure), therefore 
+// you should not invoke (as in "Meme().[...]"), use 
+// "Meme.[...]" directly.
+var Meme = function() {
+	
+	var createTextPost = function(content) {
+		return createPost('text', content);
+	};
 
-Meme.unfollow = function(guid) {
-	var yqlQuery = 'DELETE FROM meme.user.following WHERE guid="' + guid + '"';
-	return Meme.internal.execute(true, yqlQuery);
-};
+	var createPhotoPost = function(content, caption) {
+		return createPost('photo', content, caption);
+	};
 
-// ======================
-// = Internal functions =
-// ======================
-Meme.internal = {};
+	var createVideoPost = function(content, caption) {
+		return createPost('video', content, caption);
+	};
 
-// Creates a post on Meme given the type provided
-Meme.internal.createPost = function(type, content, caption) {
-	var columns = 'type';
-	var values = '"' + type + '"';
-	if (content) {
-		columns += ', content';
-		values += ', "' + content + '"';
-	}
-	if (caption) {
-		columns += ', caption';
-		values += ', "' + caption + '"';
-	}
-	var yqlQuery = 'INSERT INTO meme.user.posts (' + columns + ') VALUES (' + values + ')';
-	return Meme.internal.execute(true, yqlQuery);
-};
+	var isFollowing = function(guid) {
+		if (!Ti.App.oAuthAdapter.isLoggedIn()) {
+			throw 'sAuthentication is required to run this query.';
+		}
 
-// Executes an API query that does not expect response (insert, update, delete)
-Meme.internal.execute = function(requireAuth, yqlQuery) {
-	if (requireAuth && !Ti.App.oAuthAdapter.isLoggedIn()) {
-		throw 'Authentication is required to run this query.';
-	}
-	var yqlResponse = yql.query(yqlQuery);
-	return yqlResponse.query.results.status;
-};
+		var yqlQuery = 'SELECT * FROM meme.following WHERE owner_guid=me and guid="' + guid + '"';
+		var yqlResponse = Ti.App.oAuthAdapter.getYql().query(yqlQuery);
+		if (yqlResponse.query.results) {
+			return true;
+		}
+		return false;
+	};
+
+	var follow = function(guid) {
+		var yqlQuery = 'INSERT INTO meme.user.following (guid) VALUES ("' + guid + '")';
+		return execute(true, yqlQuery);
+	};
+
+	var unfollow = function(guid) {
+		var yqlQuery = 'DELETE FROM meme.user.following WHERE guid="' + guid + '"';
+		return execute(true, yqlQuery);
+	};
+
+	// =====================
+	// = Private functions =
+	// =====================
+
+	// Creates a post on Meme given the type provided
+	var createPost = function(type, content, caption) {
+		var columns = 'type';
+		var values = '"' + type + '"';
+		if (content) {
+			columns += ', content';
+			values += ', "' + content + '"';
+		}
+		if (caption) {
+			columns += ', caption';
+			values += ', "' + caption + '"';
+		}
+		var yqlQuery = 'INSERT INTO meme.user.posts (' + columns + ') VALUES (' + values + ')';
+		return execute(true, yqlQuery);
+	};
+
+	// Executes an API query that does not expect response (insert, update, delete)
+	var execute = function(requireAuth, yqlQuery) {
+		if (requireAuth && !Ti.App.oAuthAdapter.isLoggedIn()) {
+			throw 'Authentication is required to run this query.';
+		}
+		var yqlResponse = yql.query(yqlQuery);
+		return yqlResponse.query.results.status;
+	};
+	
+	return ({
+		createTextPost: createTextPost,
+		createPhotoPost: createPhotoPost,
+		createVideoPost: createVideoPost,
+		isFollowing: isFollowing,
+		follow: follow,
+		unfollow: unfollow
+	});	
+}();
