456f2941cd0f77020bc72050646f11bc15759aea
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-12-05 23:26:18 -0200

    Created Meme module to encapsulate Meme API logic and refactored newpost.js to use it.

--- Resources/lib/meme.js
+++ Resources/lib/meme.js
@@ -1,5 +1,17 @@
 var Meme = {};
 
+Meme.createTextPost = function(content) {
+	return Meme.internal.createPost('text', content);
+};
+
+Meme.createPhotoPost = function(content, caption) {
+	return Meme.internal.createPost('photo', content, caption);
+};
+
+Meme.createVideoPost = function(content, caption) {
+	return Meme.internal.createPost('video', content, caption);
+};
+
 Meme.isFollowing = function(guid) {
 	if (!Ti.App.oAuthAdapter.isLoggedIn()) {
 		throw 'Authentication is required to run this query.';
@@ -14,21 +26,41 @@ Meme.isFollowing = function(guid) {
 };
 
 Meme.follow = function(guid) {
-	if (!Ti.App.oAuthAdapter.isLoggedIn()) {
-		throw 'Authentication is required to run this query.';
-	}
-	
 	var yqlQuery = 'INSERT INTO meme.user.following (guid) VALUES ("' + guid + '")';
-	var yqlResponse = Ti.App.oAuthAdapter.getYql().query(yqlQuery);
-	return yqlResponse.query.results.status;
+	return Meme.internal.execute(true, yqlQuery);
 };
 
 Meme.unfollow = function(guid) {
-	if (!Ti.App.oAuthAdapter.isLoggedIn()) {
+	var yqlQuery = 'DELETE FROM meme.user.following WHERE guid="' + guid + '"';
+	return Meme.internal.execute(true, yqlQuery);
+};
+
+// ======================
+// = Internal functions =
+// ======================
+Meme.internal = {};
+
+// Creates a post on Meme given the type provided
+Meme.internal.createPost = function(type, content, caption) {
+	var columns = 'type';
+	var values = '"' + type + '"';
+	if (content) {
+		columns += ', content';
+		values += ', "' + content + '"';
+	}
+	if (caption) {
+		columns += ', caption';
+		values += ', "' + caption + '"';
+	}
+	var yqlQuery = 'INSERT INTO meme.user.posts (' + columns + ') VALUES (' + values + ')';
+	return Meme.internal.execute(true, yqlQuery);
+};
+
+// Executes an API query that does not expect response (insert, update, delete)
+Meme.internal.execute = function(requireAuth, yqlQuery) {
+	if (requireAuth && !Ti.App.oAuthAdapter.isLoggedIn()) {
 		throw 'Authentication is required to run this query.';
 	}
-	
-	var yqlQuery = 'DELETE FROM meme.user.following WHERE guid="' + guid + '"';
-	var yqlResponse = Ti.App.oAuthAdapter.getYql().query(yqlQuery);
+	var yqlResponse = yql.query(yqlQuery);
 	return yqlResponse.query.results.status;
 };
