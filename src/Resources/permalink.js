45359e70c4877e1d17d2fc6f1831109accce9aa6
Antonio Carlos Silveira <acarlos1000@gmail.com>
2010-12-15 00:58:54 -0200

    commented the development var on analytics (need to uncomment before submiting to App Store and added tracking to EMail Open

--- src/Resources/permalink.js
+++ src/Resources/permalink.js
@@ -647,7 +647,7 @@ btn_share.addEventListener('touchstart', function(e) {
 				
 		        emailDialog.setSubject(messageSubject);
 
-				var messageBody = postWebView.html + '<br/>' + L('mail_message_body_source') + '<a href=' + post.url + '>' + post.url + '</a><br/><br/><a href=' + L('memeapp_url') + '>' + L('mail_message_signature') + '</a>';
+				var messageBody = postWebView.html + '<br/>' + L('mail_message_body_source') + '<a href=' + post.url + '>' + post.url + '</a><br/><br/><a href=' + L('memeapp_url') + '>' + L('mail_message_signature') + '</a><!--YWA tracking tag--><img src="http://a.analytics.yahoo.com/p.pl?a=1000671789962&js=no&x=' + analytics.EMAIL_OPEN + '" width="1" height="1" alt="" />';
 	            emailDialog.setMessageBody(messageBody);
 	            emailDialog.setHtml(true);
 	            emailDialog.setBarColor('black');
