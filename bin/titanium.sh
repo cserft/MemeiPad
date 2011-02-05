acdf63476221cf0022c462b10dbce6ab0a2f910b
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-02-05 03:25:40 -0200

    Added colored text to most of terminal output.

--- bin/titanium.sh
+++ bin/titanium.sh
@@ -3,18 +3,16 @@
 PROJECT_ROOT=${PROJECT_ROOT:-../}
 IPHONE_SDK_VERSION="4.2"
 TI_SDK_VERSION="1.5.1"
-TI_DIR="/Library/Application Support/Titanium"
+TI_DIR="/Library/Application\ Support/Titanium"
 TI_ASSETS_DIR="${TI_DIR}/mobilesdk/osx/${TI_SDK_VERSION}"
 TI_IPHONE_DIR="${TI_ASSETS_DIR}/iphone"
 TI_BUILD="${TI_IPHONE_DIR}/builder.py"
-TI_BUILD_HOOK="${PROJECT_ROOT}/bin/titanium_hook.py"
+COLORS_HOOK="${PROJECT_ROOT}/bin/colors_hook.py"
 APP_ID="com.yahoo.meme.ipad"
 APP_NAME="MemeiPad"
 APP_DEVICE="ipad"
-PYTHONPATH="${TI_IPHONE_DIR}:${PROJECT_ROOT}/bin"
 
-# execute builder directly
-# bash -c "${TI_BUILD} run ${PROJECT_ROOT}/src/ ${IPHONE_SDK_VERSION} ${APP_ID} ${APP_NAME} ${APP_DEVICE}"
+bash -c "${TI_BUILD} run ${PROJECT_ROOT}/src/ ${IPHONE_SDK_VERSION} ${APP_ID} ${APP_NAME} ${APP_DEVICE}" \
+	| perl -pe 's/^\[DEBUG\].*$/\e[35m$&\e[0m/g;s/^\[INFO\].*$/\e[36m$&\e[0m/g;s/^\[WARN\].*$/\e[33m$&\e[0m/g;s/^\[ERROR\].*$/\e[31m$&\e[0m/g;'
 
-# execute builder through titanium_hook
-PYTHONPATH=${PYTHONPATH} ${TI_BUILD_HOOK} run ${PROJECT_ROOT}/src/ ${IPHONE_SDK_VERSION} ${APP_ID} ${APP_NAME} ${APP_DEVICE}
+killall "iPhone Simulator"
