cd63dee56383aae4438247dca94a54cf73b6ddbc
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-02-06 22:16:03 -0200

    Added all cache tests and simplified assertions.

--- src/Resources/test/test_cache.js
+++ src/Resources/test/test_cache.js
@@ -1,5 +1,7 @@
 Ti.include('../lib/cache.js');
 
+jsUnity.attachAssertions();
+
 var CacheTestSuite = {
 	suiteName: 'Cache Test Suite',
 	
@@ -7,26 +9,40 @@ var CacheTestSuite = {
 		var cache = Cache();
 		
 		cache.del('deleted_key');
-		jsUnity.assertions.assertNull(cache.get('deleted_key'));
+		assertNull(cache.get('deleted_key'));
 		
 		cache.put('deleted_key', { deleted: 'obj' });
-		jsUnity.assertions.assertNotNull(cache.get('deleted_key'));
+		assertNotNull(cache.get('deleted_key'));
 		
 		cache.del('deleted_key');
-		jsUnity.assertions.assertNull(cache.get('deleted_key'));
+		assertNull(cache.get('deleted_key'));
+	},
+	
+	testCachePut: function() {
+		var cache = Cache();
+		cache.del('my_fake_key');
+		assertNull(cache.get('my_fake_key'));
+		
+		var obj = { my: 'obj' };
+		cache.put('my_fake_key', obj);
+		
+		cached_obj = cache.get('my_fake_key');
+		assertNotNull(cached_obj);
+		assertEqual(obj, cached_obj);
 	},
 	
 	testCacheGet: function() {
-		var cache = Cache();
-		cache.del('my_fake_key');
+		var cache = Cache();		
+		cache.del('my_cache_key');
+		assertNull(cache.get('my_cache_key'));
 		
-		jsUnity.assertions.assertNull(cache.get('my_fake_key'));
+		var obj = { prop: 'value', other_prop: 'other value' };
+		cache.put('my_cache_key', obj);
 		
-		var obj = { my: 'obj' };
-		cache.put('fake_key', obj);
-		
-		cached_obj = cache.get('fake_key');
-		jsUnity.assertions.assertNotNull(cached_obj);
-		jsUnity.assertions.assertEqual(obj, cached_obj);
+		cached_obj = cache.get('my_cache_key');
+		assertNotNull(cached_obj);
+		assertEqual(obj, cached_obj);
+		assertEqual(obj.prop, cached_obj.prop);
+		assertEqual(obj.other_prop, cached_obj.other_prop);
 	}
 };
