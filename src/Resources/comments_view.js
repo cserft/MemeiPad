cdf048a685a4464e5b0a04aa283c9e55b3a88599
Antonio Carlos Silveira <acarlos1000@gmail.com>
2011-02-07 18:13:27 -0200

    implementing the comment submit

--- src/Resources/comments_view.js
+++ src/Resources/comments_view.js
@@ -59,9 +59,40 @@ var btn_send_comment2 = Titanium.UI.createButton({
 });
 commentBoxView.add(btn_send_comment2);
 
+// Send COMMENT
+btn_send_comment2.addEventListener("click", function(e) {
+	if (commentField.value != '') {
+		var ok = Ti.App.meme.createComment(_guid, _pubId, commentField.value);
+		
+		//removes the label to add the animation
+		btn_send_comment2.title = "saving...";
+		
+		if (ok) {
+			//Analytics Request
+			doYwaRequest(analytics.ADD_COMMENT);
+			
+			setTimeout(function()
+			{
+				btn_send_comment2.title = "done!";
+				
+			// Close Keyboard
+			// Empty commentField
+			// Set btn_send_comment2.title to original string
+
+			},2000);
+
+		} else {
+			Ti.API.info("Error while saving Commenting");	
+		}
+	} else {
+		Ti.UI.createAlertDialog({
+			title: L('comment_alert_empty_title'),
+			message: L('comment_alert_empty_message')
+		}).show();
+	}
+});
+
 // Comments Tableview
-// var data = [];
-// var lastRow = 10;
 
 var commentsTableView = Ti.UI.createTableView({
 	backgroundColor: 	'transparent',
@@ -131,49 +162,49 @@ function getComments(comments) {
 
 			var avatar = Ti.UI.createImageView({
 				image: item.userInfo.avatar_url.thumb,
-				backgroundColor: 'black',
-				height:40,
-				width:40,
-				top:10,
-				left:10,
+				height: 40,
+				width: 40,
+				top: 24,
+				left: 13,
 				defaultImage:'images/default_img_avatar.png'
 			});
-			
 			row.add(avatar);
-
-			var username = Ti.UI.createLabel({
-				text: item.userInfo.name,
-				color: '#863486',
-				width: 250,
-				height:15,
-				top: 8,
-				left:67,
-				textAlign:'left',
-				font:{fontSize:12, fontFamily:'Helvetica', fontWeight:'bold'}
-			});
-			row.add(username);
 			
 			var quote_icon = Ti.UI.createImageView({
 				image : 		'images/quote_innerhtml.png',
 				height: 		14,
 				width: 			16,
 				top: 			24,
-				left: 			60
+				left: 			62
 			});
 			row.add(quote_icon); 
 
 			var commentTxt = Ti.UI.createLabel({
-				// backgroundColor: 	'red',
 				text: 			strip_html_entities(item.comment),
 				color: 			'#333',
 				height: 		52,
 				width: 			620,
 				top: 			26,
-				left: 			86,
+				left: 			88,
 				textAlign: 		'left',
 				font: 			{fontSize:14,fontFamily:'Georgia', fontStyle:'italic'},
 			});
 			row.add(commentTxt);
+			
+			var title_width = item.userInfo.title.length * 7;
+			
+			var username = Ti.UI.createLabel({
+				text: 					item.userInfo.title,
+				// backgroundColor: 		'red',
+				color: 					'#863486',
+				width: 					title_width,
+				height: 				23,
+				top: 					68,
+				left: 					88,
+				textAlign: 				'left',
+				font: 					{fontSize:12, fontFamily:'Helvetica', fontWeight:'bold'}
+			});
+			row.add(username);
 			
 			var commentTime = Ti.UI.createLabel({
 				// backgroundColor: 	'yellow',
@@ -182,7 +213,7 @@ function getComments(comments) {
 				height: 		23,
 				width: 			150,
 				top: 			68,
-				left: 			220,
+				left: 			username.left + title_width + 10,
 				textAlign: 		'left',
 				font: 			{fontSize:12,fontFamily:'Helvetica',fontWeight:'regular'},
 			});
