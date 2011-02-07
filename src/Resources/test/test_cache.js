aefccc4decc1a6846873a24f8ca2f881f299e8dd
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-02-06 22:10:21 -0200

    Added test for cache delete.

--- src/Resources/test/test_cache.js
+++ src/Resources/test/test_cache.js
@@ -3,8 +3,23 @@ Ti.include('../lib/cache.js');
 var CacheTestSuite = {
 	suiteName: 'Cache Test Suite',
 	
+	testCacheDelete: function() {
+		var cache = Cache();
+		
+		cache.del('deleted_key');
+		jsUnity.assertions.assertNull(cache.get('deleted_key'));
+		
+		cache.put('deleted_key', { deleted: 'obj' });
+		jsUnity.assertions.assertNotNull(cache.get('deleted_key'));
+		
+		cache.del('deleted_key');
+		jsUnity.assertions.assertNull(cache.get('deleted_key'));
+	},
+	
 	testCacheGet: function() {
 		var cache = Cache();
+		cache.del('my_fake_key');
+		
 		jsUnity.assertions.assertNull(cache.get('my_fake_key'));
 		
 		var obj = { my: 'obj' };
@@ -13,5 +28,5 @@ var CacheTestSuite = {
 		cached_obj = cache.get('fake_key');
 		jsUnity.assertions.assertNotNull(cached_obj);
 		jsUnity.assertions.assertEqual(obj, cached_obj);
-	}	
+	}
 };
