5994a428d850a51735279e0a6e280c6d2319e1e9
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-02-11 12:12:40 -0200

    Fixed language generation.

--- bin/i18n.sh
+++ bin/i18n.sh
@@ -11,13 +11,13 @@ for language in ${LANGUAGES[@]}; do
 	IN="${PROJECT_ROOT}/src/i18n/${language}/strings.xml"
 	OUT="${PROJECT_ROOT}/src/Resources/${language}.lproj/Localizable.strings"
 	
-	if [ -f "${IN}" ]; then	
-		# delete old language files
-		rm -rf $OUT
+	# delete old language files
+	rm -rf $OUT
 
-		if [ "${1}" == "clean" ]; then
-			echo "Deleted: ${OUT}"
-		else
+	if [ "${1}" == "clean" ]; then
+		echo "Deleted: ${OUT}"
+	else
+		if [ -f "${IN}" ]; then	
 			for line in `cat ${IN}`; do
 				if [[ $line =~ '<string' ]]; then
 					echo $line | sed -e 's/[[:blank:]]*<string name=//g' -e 's/">/" = "/g' -e 's/<\/string>/";/g' -e 's/<!--.*-->//g' >> $OUT
